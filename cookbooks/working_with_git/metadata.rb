f4fbf3e7bac897b649385251ee325b643eb2a0ac
rllacey <rllacey@bl.valdosta.edu>
2011-09-06 12:52:42 -0400

    test

--- cookbooks/working_with_git/metadata.rb
+++
@@ -1,6 +0,0 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
-license          "All rights reserved"
-description      "Installs/Configures working_with_git"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.1"
